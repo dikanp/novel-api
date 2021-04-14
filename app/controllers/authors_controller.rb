ed484217d86d4240185ab4115950a6daa149869e
dikanp <andika.dikanp@gmail.com>
2021-04-14 21:27:26 +0700

    delete author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,12 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :notFound
+  def notFound
+    render json: {
+      values: {},
+      message: "Data not Found!",
+    }, status: 400
+  end
+
   def index
     @authors = Author.all
     render json: {
@@ -33,6 +41,36 @@ class AuthorsController < ApplicationController
       render json: {
         values: "",
         message: @author.errors.full_messages
+      }, status: 400
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(params.permit(:name))
+      render json: {
+        values: @author,
+        message: "success"
+      }, status: 200
+    else
+      render json: {
+        values: "",
+        message: @author.errors.full_messages
+      }, status: 400
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    if @author.destroy
+      render json: {
+        values: @author,
+        message: "success"
+      }, status: 200
+    else
+      render json: {
+        values: "",
+        message: "Failed"
       }, status: 400
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'authors', to: 'authors#index'
   get 'authors/:id', to: 'authors#show'
   post 'authors', to: 'authors#create'
+  put 'authors/:id', to: 'authors#update'
+  delete 'authors/:id', to: 'authors#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
